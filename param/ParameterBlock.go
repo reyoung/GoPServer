58698af3f0b1a92936468d9c9d0b8d339fe667ae
Yu Yang <yuyang18@baidu.com>
2016-12-03 20:06:49 +0800

    Refine

--- param/ParameterBlock.go
+++ param/ParameterBlock.go
@@ -3,6 +3,14 @@ package param
 import "github.com/reyoung/GoPServer/protocol"
 import flatbuffers "github.com/google/flatbuffers/go"
 import "sync"
+import "errors"
+
+// ErrCannotParsePayload is the error when parameterBlock cannot parsing payload
+var ErrCannotParsePayload = errors.New("Error when parsing request payload.")
+
+var ErrParamBlockDimMismatch = errors.New("This Parameter block has been inited with different parameter dim")
+
+var ErrParamBlockBufferInited = errors.New("Current ParamBlock Id has been inited")
 
 type blockJob struct {
 	req      *protocol.ParameterServerRequest
@@ -12,7 +20,8 @@ type blockJob struct {
 }
 
 type parameterBlock struct {
-	buffer map[byte][]float64 // Buffers
+	buffer map[int8][]float64 // Buffers
+	dim    uint32
 	job    chan *blockJob
 }
 
@@ -47,7 +56,7 @@ func (param *Parameters) DoJob(req *protocol.Requests) *flatbuffers.Builder {
 		block, ok := param.params[n]
 		if !ok {
 			block = &parameterBlock{
-				buffer: make(map[byte][]float64),
+				buffer: make(map[int8][]float64),
 				job:    make(chan *blockJob, 10),
 			}
 			go block.process() // each parameter block use a goroutine.
@@ -74,6 +83,57 @@ func (param *Parameters) DoJob(req *protocol.Requests) *flatbuffers.Builder {
 	return builder
 }
 
-func (block *parameterBlock) process() {
+func createParameterBlockDone(name string, builder *flatbuffers.Builder, lock *sync.Mutex) flatbuffers.UOffsetT {
+	lock.Lock()
+	defer lock.Unlock()
+	errOffset := builder.CreateString("ok")
+	protocol.CreateResponseStart(builder)
+	protocol.CreateResponseAddError(builder, errOffset)
+	payloadOffset := protocol.CreateResponseEnd(builder)
 
+	nameOffset := builder.CreateString(name)
+	protocol.ParameterServerResponseStart(builder)
+	protocol.ParameterServerResponseAddName(builder, nameOffset)
+	protocol.ParameterServerResponseAddPayloadType(builder, protocol.ResponsePayLoadCreateResponse)
+	protocol.ParameterServerResponseAddPayload(builder, payloadOffset)
+	return protocol.ParameterServerResponseEnd(builder)
+}
+
+func (block *parameterBlock) process() error {
+	table := new(flatbuffers.Table)
+	createReq := new(protocol.CreateRequest)
+	for {
+		job, ok := <-block.job
+		if !ok {
+			break
+		}
+
+		switch job.req.PayloadType() {
+		case protocol.RequestPayLoadCreateRequest:
+			ok = job.req.Payload(table)
+			if !ok {
+				return ErrCannotParsePayload
+			}
+
+			createReq.Init(table.Bytes, table.Pos)
+			if block.dim == 0 {
+				block.dim = createReq.Dim()
+			} else if block.dim != createReq.Dim() {
+				return ErrParamBlockDimMismatch
+			}
+
+			_, ok = block.buffer[createReq.Id()]
+			if ok {
+				return ErrParamBlockBufferInited
+			}
+			buf := make([]float64, createReq.Size())
+			for i := 0; i < len(buf); i++ {
+				buf[i] = 0.0
+			}
+			block.buffer[createReq.Id()] = buf
+			// CreateDone, Write Return Value.
+			job.onFinish(createParameterBlockDone(string(job.req.Name()), job.builder, job.lock))
+		}
+	}
+	return nil
 }
